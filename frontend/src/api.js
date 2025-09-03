c48a8ef7f8c6ac5c4e4f4d37f67270fef92cdc1e
Nazif Bin Morshed <134962453+NajifVai69@users.noreply.github.com>
2025-09-04 03:59:19 +0600

    donno

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: 'food-order-app-production-7604.up.railway.app/api' || 'http://localhost:5000/api',
   withCredentials: true, // Important for cookie-based authentication
 });
 
